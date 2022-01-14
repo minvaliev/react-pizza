e4f5db5290b54dae658e09a324bf569f316fe3f9
Albert <a.minvaliev@mosproektkompleks.ru>
2022-01-14 12:07:22 +0300

    Реализовал компонент сортировки пиццы и реализована страница корзины (react-router)

--- src/index.js
+++ src/index.js
@@ -3,10 +3,14 @@ import ReactDOM from 'react-dom';
 import './scss/app.scss';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter as Router } from 'react-router-dom'
+import Header from './components/Header'
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Router>
+        <App/>
+    </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
